897c99670d95755846bacc824d2bb5fd076c4574
quetee <124061508+qtTF2@users.noreply.github.com>
2023-04-23 02:24:38 +0200

    Update version.js

--- plugins/update/version.js
+++ plugins/update/version.js
@@ -1,4 +1,4 @@
-var latestVersion = "0.5.3";
+var latestVersion = "0.5.4";
 let panelversion = document.getElementById("ver")
 let panelversio2 = document.getElementById("ver2")
 panelversio2.textContent = latestVersion
